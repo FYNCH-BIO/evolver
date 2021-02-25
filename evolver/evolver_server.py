c9f07278a819341efc9cbd28a735e38d39e8b091
Zachary Heins <zackheins@gmail.com>
2021-02-25 08:43:52 -0500

    Increasing serial delays

--- evolver/evolver_server.py
+++ evolver/evolver_server.py
@@ -276,7 +276,7 @@ def serial_communication(param, value, comm_type):
     serial_output = param + ','.join(output) + ',' + evolver_conf['serial_end_outgoing']
     print(serial_output)
     serial_connection.write(bytes(serial_output, 'UTF-8'))
-    time.sleep(.05)
+    time.sleep(evolver_conf['serial_delay'])
 
     # Read and process the response
     response = serial_connection.readline().decode('UTF-8', errors='ignore')
@@ -306,7 +306,7 @@ def serial_communication(param, value, comm_type):
     serial_connection.write(bytes(serial_output, 'UTF-8'))
 
     # This is necessary to allow the ack to be fully written out to samd21 and for them to fully read
-    time.sleep(.05)
+    time.sleep(evolver_conf['serial_delay'])
 
     if returned_data[0] == evolver_conf['data_response_char']:
         returned_data = returned_data[1:]
